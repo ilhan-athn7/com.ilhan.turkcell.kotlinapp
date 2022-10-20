67629f12a2ff1b63224b1b0d6a38ba318fb47bb9
ilhan-athn7 <ata.ilhan.official@gmail.com>
2022-10-20 19:55:53 +0300

    Sayısal, metinsel ve mantıksal değişkenler tanımlanıp logcat'a yazdırıldı.

--- app/src/main/java/com/ilhan/turkcell/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/ilhan/turkcell/kotlinapp/MainActivity.kt
@@ -7,5 +7,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Örnek değişken
+        var exampleVar = 5
+
+        //Sayısal, Tamsayı (Int)
+        var exampleInt : Int
+        exampleInt = 7
+
+        //Sayısal, Kesirli sayı (Float-Double)
+        var exampleFloat : Float = 32.5f
+        var exampleDouble : Double = 61.5
+
+        //Metinsel (String)
+        var exampleString : String = "Test"
+        var exampleString2 : String = "Done"
+        var mixedVar : String = exampleString + exampleString2
+
+        //Mantıksal (Boolean)
+        var exampleBoolean : Boolean //= true
+        exampleBoolean = 3 < 5 //true döndürür
+        //exampleBoolean = 3 > 5 //false döndürür
+
+        //Logcat'a yazdırma
+        println(exampleVar)
+        println(exampleInt)
+        println(exampleFloat)
+        println(exampleDouble)
+        println(exampleString + " " + exampleString2)
+        println(mixedVar)
+        println(exampleBoolean)
+
     }
 }
