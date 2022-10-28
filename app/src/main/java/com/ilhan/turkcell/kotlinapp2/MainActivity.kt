3bac65ef888aa49f40ae9111792f09d2d5074e92
ilhan-athn7 <ata.ilhan.official@gmail.com>
2022-10-28 21:07:07 +0300

    Veri dizileri incelendi.

--- app/src/main/java/com/ilhan/turkcell/kotlinapp2/MainActivity.kt
+++ app/src/main/java/com/ilhan/turkcell/kotlinapp2/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Veri dizileri
+        var exampleSet1 = arrayOf("value1","value2",3,4)
+        var exampleSet2 = intArrayOf(3,7,2,1,4)
+
+        println(exampleSet1[1])
+        println(exampleSet2.get(2))
+
+        exampleSet1[0] = "valueNew1"
+        println(exampleSet1[0])
+
+        exampleSet1.set(1,"valueNew2")
+        println(exampleSet1[1])
+
     }
 }
